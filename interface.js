4d2bd7f8ef4c8dc65e8db5558f5192e528769b81
redballoon <fred@Edmeisters-MacBook-Air.local>
2015-11-25 03:06:45 -0500

    edit

--- interface.js
+++ interface.js
@@ -1,6 +1,7 @@
 var cylon = require('cylon');
-var fft = require('fft-js').fft;
-var fft_util = require('fft-js').util;
+var speaker = require('speaker');
+//var fft = require('fft-js').fft;
+//var fft_util = require('fft-js').util;
 var fs = require("fs");
 
 
@@ -15,15 +16,35 @@ var options = {
 };
 cylon.robot({
 	connections: {
+		arduino: { adaptor : 'firmata', port: '/dev/cu.usbmodem1421' },
 		audio: { adaptor: 'audio' }
 	},
 	devices: {
-		audio: { driver: 'audio' }
+		audio: { driver: 'audio' },
+		pin: { driver: 'direct-pin', pin : 4 }
 	},
 	work: function(my) {
-		my.audio.on('playing', function(song){
-			console.log('Playing this nice tune: "' + song + '"');
+		//my.audio.on('playing', function(song){
+			//console.log('Playing this nice tune: "' + song + '"');
+		//});
+		
+		
+		var path = './assets/audio-sample.mp3';
+		
+		my.connections.audio.speaker = new speaker({
+			channels: 1,
+			bitDepth: 16,
+			sampleRate: 88200
 		});
+		
+		var buff = new Buffer(1024 * ((16 / 8) * 1));
+		
+		//my.connections.audio.play(path);
+		
+		my.pin.analogWrite(1);
+		
+		//console.log();
+		
 		//44100
 		// 2 channels
 		//bitDepth 16
@@ -31,10 +52,11 @@ cylon.robot({
 		
 		//console.log(my.audio.connection.decoder);
 		
-		var path = './assets/audio-sample.mp3';
+		
+		
 		//my.audio.play(path);
 		
-		
+		/*
 		var readStream = fs.createReadStream(path);
 		readStream.pipe(my.audio.connection.decoder);
 		
@@ -49,6 +71,7 @@ cylon.robot({
 			
 			console.log(results);
 		}, 2000);
+		*/
 		//this.emit("playing", trackPath);
 		
 	}
